0034cd8fc42d9300aae6ccf3c0e6179ce3b329bf
parsajay <parsajay.dev@gmail.com>
2023-10-17 18:41:26 +0330

    added a height slider for second row

--- lib/body.dart
+++ lib/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/cupertino.dart';
 
 class Body extends StatefulWidget {
   const Body({super.key});
@@ -14,6 +15,18 @@ class _BodyState extends State<Body> {
   final Color sexButtonDefultColor = Colors.white;
   final Color onPressedColor = Colors.redAccent;
   String userSex = 'male';
+  double usersHeightValue = 172;
+
+  final BoxDecoration rowDecoration = const BoxDecoration(
+      color: Color.fromRGBO(35, 32, 32, 1),
+      borderRadius: BorderRadius.all(Radius.circular(8.0)),
+      boxShadow: <BoxShadow>[
+        BoxShadow(
+          color: Color.fromRGBO(88, 88, 88, 0.9),
+          offset: Offset(2, 2),
+          blurRadius: 8,
+        )
+      ]);
 
   @override
   Widget build(BuildContext context) {
@@ -25,23 +38,14 @@ class _BodyState extends State<Body> {
         // first row
 
         Container(
-          height: 140,
+          height: 160,
           child: Row(
             children: [
               //male button
               Flexible(
                 fit: FlexFit.tight,
                 child: Container(
-                  decoration: const BoxDecoration(
-                      color: Color.fromRGBO(35, 32, 32, 1),
-                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                      boxShadow: <BoxShadow>[
-                        BoxShadow(
-                          color: Color.fromRGBO(88, 88, 88, 0.9),
-                          offset: Offset(2, 2),
-                          blurRadius: 8,
-                        )
-                      ]),
+                  decoration: rowDecoration,
                   margin: const EdgeInsets.fromLTRB(8, 8, 4, 8),
                   padding: const EdgeInsets.all(20),
                   child: TextButton(
@@ -114,8 +118,44 @@ class _BodyState extends State<Body> {
 
         // second row
         Container(
-          height: 140,
-          child: Row(),
+          decoration: rowDecoration,
+          margin: const EdgeInsets.all(8),
+          padding: const EdgeInsets.all(30),
+          height: 160,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  const Text('Height',
+                      style: TextStyle(color: Colors.white, fontSize: 24)),
+                  Row(
+                    children: [
+                      Text(usersHeightValue.toString().substring(0, 3),
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 20)),
+                      const Text(
+                        'cm',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  CupertinoSlider(
+                    onChanged: (c) {
+                      setState(() {
+                        usersHeightValue = c;
+                      });
+                    },
+                    value: usersHeightValue,
+                    activeColor: Colors.redAccent,
+                    min: 120,
+                    max: 240,
+                    divisions: 120,
+                  )
+                ],
+              )
+            ],
+          ),
         ),
 
         // third row
