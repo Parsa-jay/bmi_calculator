2ad0115381b033a7cdb4f0001df757d212242b75
parsajay <parsajay.dev@gmail.com>
2023-10-26 23:39:46 +0330

    separated height slider from body widget in its own file

--- lib/body.dart
+++ lib/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter/cupertino.dart';
+//import 'package:flutter/cupertino.dart';
+import 'height.dart';
 
 class Body extends StatefulWidget {
   const Body({super.key});
@@ -15,7 +16,6 @@ class _BodyState extends State<Body> {
   final Color sexButtonDefultColor = Colors.white;
   final Color onPressedColor = Colors.redAccent;
   String userSex = 'male';
-  double usersHeightValue = 172;
 
   final BoxDecoration rowDecoration = const BoxDecoration(
       color: Color.fromRGBO(35, 32, 32, 1),
@@ -116,46 +116,13 @@ class _BodyState extends State<Body> {
           ),
         ),
 
-        // second row
+        // second row, height slider
         Container(
           decoration: rowDecoration,
           margin: const EdgeInsets.all(8),
           padding: const EdgeInsets.all(30),
           height: 160,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                children: [
-                  const Text('Height',
-                      style: TextStyle(color: Colors.white, fontSize: 24)),
-                  Row(
-                    children: [
-                      Text(usersHeightValue.toString().substring(0, 3),
-                          style: const TextStyle(
-                              color: Colors.white, fontSize: 20)),
-                      const Text(
-                        'cm',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ],
-                  ),
-                  CupertinoSlider(
-                    onChanged: (c) {
-                      setState(() {
-                        usersHeightValue = c;
-                      });
-                    },
-                    value: usersHeightValue,
-                    activeColor: Colors.redAccent,
-                    min: 120,
-                    max: 240,
-                    divisions: 120,
-                  )
-                ],
-              )
-            ],
-          ),
+          child: const HeightSlider(),
         ),
 
         // third row
